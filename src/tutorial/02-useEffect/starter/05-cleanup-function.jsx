cca2bf6c3090b50026d4f720ae4775c6506898d0
Tibor <tibor736@hotmail.com>
2023-07-08 16:43:10 +0300

    08: UseEffect toggling every time when component toggled from click instead of oonce on initial render

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+	const [value, setValue] = useState(false);
+
+	return (
+		<div>
+			<button className="btn" onClick={() => setValue(!value)}>
+				toggle component
+			</button>
+			{value && <NewComponent />}
+		</div>
+	);
+};
+
+const NewComponent = () => {
+	useEffect(() => {
+		console.log("RANDOM");
+	}, []);
+
+	return <h4 className="alert alert-danger">Toggled</h4>;
 };
 
 export default CleanupFunction;
