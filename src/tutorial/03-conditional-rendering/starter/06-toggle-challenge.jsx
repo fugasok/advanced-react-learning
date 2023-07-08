163559620398995b3d92ad4ba98054ce02cdf7b9
Tibor <tibor736@hotmail.com>
2023-07-08 15:11:46 +0300

    07: User login/logout challenge done

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+	const [value, setValue] = useState(false);
+
+	// const clickHandler = () => {
+	// 	if (value) {
+	// 		setValue(false);
+	// 	}
+	// 	setValue(true);
+	// };
+
+	return (
+		<div>
+			<button className="btn" onClick={() => setValue(!value)}>Check value</button>
+			{value && <ValueComponent />}
+		</div>
+	);
+};
+
+const ValueComponent = () => {
+	return <div className="alert alert-danger">VALUE TRUE</div>;
 };
 
 export default ToggleChallenge;
--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+	const [user, setUser] = useState(null);
+
+	const login = () => {
+		setUser({ name: "Peter" });
+	};
+
+	return !user ? (
+		<div>
+			<h4>Please login</h4>
+			<button className="btn" onClick={login}>
+				Login
+			</button>
+		</div>
+	) : (
+		<div>
+			<UserInfo user={user} />
+			<button className="btn" onClick={() => setUser(null)}>
+				logout
+			</button>
+		</div>
+	);
+};
+
+const UserInfo = ({ user }) => {
+	return (
+		<div>
+			<h4>
+				Hello there <i>{user.name}</i>
+			</h4>
+		</div>
+	);
 };
 
 export default UserChallenge;
