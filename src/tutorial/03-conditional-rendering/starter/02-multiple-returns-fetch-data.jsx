c209a731cbad752670c02161e6f7f13c2bf889a0
Tibor <tibor736@hotmail.com>
2023-07-08 13:13:53 +0300

    05:multiple returns destructure object

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -10,6 +10,11 @@ const MultipleReturnsFetchData = () => {
 		const fetchUser = async () => {
 			try {
 				const response = await fetch(url);
+				if (!response.ok) {
+					setIsError(true);
+					setIsLoading(false);
+					return;
+				}
 				const user = await response.json();
 				setUser(user);
 			} catch (error) {
@@ -28,16 +33,18 @@ const MultipleReturnsFetchData = () => {
 		return <h2>There was an error...</h2>;
 	}
 
+	const { avatar_url, name, company, bio } = user;
+
 	return (
 		<div>
 			<img
 				style={{ width: "150px", borderRadius: "25px" }}
-				src={user.avatar_url}
-				alt={user.name}
+				src={avatar_url}
+				alt={name}
 			/>
 			<h2>{user.name}</h2>
-			<h4>works at {user.company}</h4>
-			<p>{user.bio}</p>
+			<h4>works at {company}</h4>
+			<p>{bio}</p>
 		</div>
 	);
 };
