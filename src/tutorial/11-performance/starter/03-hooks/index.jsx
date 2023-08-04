e5dff32a6cda46d7773319d332148e8db3a35c91
Tibor <tibor736@hotmail.com>
2023-08-04 15:47:09 +0300

    slowFunction example

--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,14 +1,21 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { data } from "../../../../data";
 import List from "./List";
+import slowFunction from "./slowFunction";
 const LowerState = () => {
 	const [people, setPeople] = useState(data);
 	const [count, setCount] = useState(0);
 
-	const removePerson = useCallback((id) => {
-		const newPeople = people.filter((person) => person.id !== id);
-		setPeople(newPeople);
-	}, [people]);
+	const value = useMemo(() => slowFunction(), []);
+	console.log(value);
+
+	const removePerson = useCallback(
+		(id) => {
+			const newPeople = people.filter((person) => person.id !== id);
+			setPeople(newPeople);
+		},
+		[people]
+	);
 
 	return (
 		<section>
